b6df792f8a0791badca992c2463afc78cb381746
Einstein-msk <nikitadidukh10@gmail.com>
2024-03-28 22:28:37 +0300

    commit-14 change App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from "./Post/Post";
+import User from "./User/User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    //add summ
+    const summ = (a: number, b: number) => {
+        return a + b;
+    }
+    return (
+        <div className="App">
+            <Post/>
+            <User/>
+            <p>{summ(2,3)}</p>
 
-      </header>
-    </div>
-  );
+
+        </div>
+    );
 }
 
 export default App;
